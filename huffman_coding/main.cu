6883add4f2e4449d7b880b6db79da0df1da9e7a3
Fabian, Daniel <dfabian5@kent.edu>
2020-03-08 19:34:53 -0400

    finished midterm project

---
+++ huffman_coding/main.cu
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// FILE:        main.cu
+// DESCRIPTION: huffman encodes a text file and outputs its encoding
+// AUTHOR:      Dan Fabian
+// DATE:        3/6/2020
+
+#include "encoder.h"
+
+int main()
+{
+    Encoder coder;
+    coder.encode();
+}
